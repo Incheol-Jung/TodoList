6edd9ca85d8b351a1eb0d761bb76b52c6139e7c3
incheol_KOR <incheol@fashiongo.com>
2018-12-04 14:04:51 +0900

    update rest api method

--- src/main/java/com/homework/todolist/controller/TodoListController.java
+++ src/main/java/com/homework/todolist/controller/TodoListController.java
@@ -6,14 +6,17 @@ package com.homework.todolist.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Incheol Jung
  */
 @Controller
 public class TodoListController {
+	
+	@ResponseBody
 	@RequestMapping(value="/", method=RequestMethod.GET)
     public String getTodoList() {
-        return "shortener";
+        return "TodoList";
     }
 }
